2895ef74ee1b7b12b17c5fa4c703794cf6097c7c
KowyXYZ <kowybusiness@gmail.com>
2024-08-29 09:23:02 +0200

    user actions and providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 import { Metadata } from "next"
 import { ClerkProvider } from "@clerk/nextjs"
 import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,16 +25,19 @@ export default function RootLayout({ children }: {children: React.ReactNode}) {
       baseTheme: dark,
       variables: {colorPrimary: '#3371FF', fontSize: '16px'}
     }}>
-    <html lang="en" suppressHydrationWarning>
-          <body
-            className={cn(
-              "min-h-screen  font-sans antialiased",
-              fontSans.variable
-            )}
-          >
-            {children}
-          </body>
-    </html>
+        <html lang="en" suppressHydrationWarning>
+              <body
+                className={cn(
+                  "min-h-screen  font-sans antialiased",
+                  fontSans.variable
+                )}
+              >
+                <Provider>
+                  {children}
+                </Provider>
+                
+              </body>
+        </html>
     </ClerkProvider>
   
   )
